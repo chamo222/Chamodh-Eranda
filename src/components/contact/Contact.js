70acc06f4488931b8cdd139f5d9fc6cc9200e6c2
Chamodh Eranda <chamodheranda51@gmail.com>
2025-06-30 23:27:31 +0530

    changes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -69,11 +69,11 @@ const Contact = () => {
       <Title title="Get" subTitle="in Touch" />
       <div className="p-6 w-full flex flex-col md:flex-row justify-between gap-4 md:gap-10 lgl:gap-20">
         <div className="w-full lgl:w-1/2">
-          <p className="flex gap-6 justify-between w-full text-lg text-[#ccc] py-4 border-b-[1px] border-b-zinc-800">
+          <p className="flex gap-6 justify-between w-full text-md text-[#ccc] py-4 border-b-[1px] border-b-zinc-800">
             <span className="bg-designColor text-gray-700 text-sm font-titleFont font-medium px-2 rounded-md flex items-center justify-center">
               Address:
             </span>
-            Muscat, Oman
+             Gonahena,Kadawatha
           </p>
           <p className="flex justify-between w-full text-lg text-[#ccc] py-4 border-b-[1px] border-b-zinc-800">
             <span className="bg-designColor text-gray-700 text-sm font-titleFont font-medium px-2 rounded-md flex items-center justify-center">
