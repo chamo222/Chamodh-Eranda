d3d7d7a9f7ea95494e216fe8a9021587235a00ef
Chamodh Eranda <chamodheranda51@gmail.com>
2025-07-16 10:03:16 +0530

    changes

--- src/components/about/FunFact.js
+++ src/components/about/FunFact.js
@@ -9,7 +9,7 @@ const FunFact = () => {
   return (
     <div className="px-0 grid grid-cols-2 md:grid-cols-4 pb-10">
       <FunFactCard icon={<BsTrophyFill />} des="5 Awards Won" />
-      <FunFactCard icon={<IoLogoInstagram />} des="678 Followers" />
+      <FunFactCard icon={<IoLogoInstagram />} des="680 Followers" />
       <FunFactCard icon={<SiAntdesign />} des="8 Finished Projects" />
       <FunFactCard icon={<BiCodeAlt />} des="100 hours of coding" />
     </div>
